ea0f51310c787a33539ad5db975d32314b3100d9
Ashley Allen <95989498+aallen5636@users.noreply.github.com>
2022-02-02 12:37:31 -0600

    Create population-dispersion.py

---
+++ population-dispersion.py
@@ -0,0 +1,12 @@
+import statistics
+
+statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+2.25
+
+statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+1.5
+
+import math
+
+math.sqrt(statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+1.5
